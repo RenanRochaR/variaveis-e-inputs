cdb5a058242a2983cbf67fb86d33447c65fb61f9
RenanRochaR <renan123rocha@Outlook.com>
2023-05-31 11:09:56 -0300

    variaveis e inputs

---
+++ Olá_Mundo/variaveis-e-input/app.js
@@ -0,0 +1,13 @@
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const minhaPrimeiraConstanteString = 'Minha Primeira Constante' ;
+console.log(minhaPrimeiraConstanteString);
+let leituraDeCampo;
+readLine.question('Informe sua Idade:', input => {
+    leituraDeCampo = input;
+    console.log(`o usuario digitou: ${leituraDeCampo}`);
+});
+
